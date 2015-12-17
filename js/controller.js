f1efed09bd1cecc7e96310d1cca69608b760c0d9
rostasan <ross.dickman@gmail.com>
2015-12-16 20:36:05 -0600

    Form Validation

--- js/controller.js
+++ js/controller.js
@@ -31,31 +31,48 @@ function appCtrl($scope){
                             {name: "Hannah"},
                             {name: "Aaliyah"},
                             {name: "Evelyn"},
-                            {name: "Addison"} 
-                            ];
-    /*        $scope.xjobtitle = "";
-        $scope.xtedioustask = "";
-        $scope.xdirtytask = "";
-        $scope.xcelebrity = "";
-        $scope.xuselessskill = "";
-        $scope.xadjective = "";
-        $scope.xobnoxiuouscelbertity = "";
-        $scope.xhugenumber = "";
-       $scope.madLib = function(){
-            console.log("Submitting worked");
-                $scope.femalename = $scope.selected.xfemalenames.name;
-                $scope.jobtitle = $scope.xjobtitle;
-                $scope.tedioustask = $scope.xtedioustask;
-                $scope.dirtytask = $scope.xdirtytask;
-                $scope.celebrity = $scope.xcelebrity;
-                $scope.uselessskill = $scope.xuselessskill;
-                $scope.adjective = $scope.xadjective;
-                $scope.obnoxiuouscelbertity = $scope.xobnoxiuouscelbertity;
-                $scope.hugenumber = $scope.xhugenumber;
-            }; */
-        };
-     function resetForm($scope){
-         $scope.resetForm = null;
-         
-     }   
+                            {name: "Addison"}
+    ];
+        $scope.resetForm = [];
+
+
+        $scope.reset = function(madForm) {
+            $scope.femalenames = angular.copy($scope.resetForm);
+            $scope.jobtitle = angular.copy($scope.resetForm);
+            $scope.tedioustask = angular.copy($scope.resetForm);
+            $scope.dirtytask = angular.copy($scope.resetForm);
+            $scope.celebrity = angular.copy($scope.resetForm);
+            $scope.uselessskill = angular.copy($scope.resetForm);
+            $scope.adjective = angular.copy($scope.resetForm);
+            $scope.obnoxiuouscelbertity = angular.copy($scope.resetForm);
+            $scope.hugenumber = angular.copy($scope.resetForm);
+            madForm.$setPristine()
+    };
+
+
+
+        }
+
     })();
+
+
+    /*        $scope.xjobtitle = "";
+ $scope.xtedioustask = "";
+ $scope.xdirtytask = "";
+ $scope.xcelebrity = "";
+ $scope.xuselessskill = "";
+ $scope.xadjective = "";
+ $scope.xobnoxiuouscelbertity = "";
+ $scope.xhugenumber = "";
+ $scope.madLib = function(){
+ console.log("Submitting worked");
+ $scope.femalename = $scope.selected.xfemalenames.name;
+ $scope.jobtitle = $scope.xjobtitle;
+ $scope.tedioustask = $scope.xtedioustask;
+ $scope.dirtytask = $scope.xdirtytask;
+ $scope.celebrity = $scope.xcelebrity;
+ $scope.uselessskill = $scope.xuselessskill;
+ $scope.adjective = $scope.xadjective;
+ $scope.obnoxiuouscelbertity = $scope.xobnoxiuouscelbertity;
+ $scope.hugenumber = $scope.xhugenumber;
+ }; */
